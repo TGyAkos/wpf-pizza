a1cbe229e7b26456430aad4811f5ac8aea03a7cb
Tanulo <Tanulo@DESKTOP-K4VUJ70>
2024-04-24 11:04:24 +0200

    Add project files.

---
+++ PizzaOrder/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaOrder
+{
+    public class Pizza
+    {
+        string name { get; set; }
+        int cost { get; set; }
+        Souce souce { get; set; }
+        List<String> intigers { get; set; }
+    }
+    public class Souce
+    {
+        string name { get; set; }
+        int cost { get; set; 
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
